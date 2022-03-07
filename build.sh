ab3620421fc959b84b751b56e1d71c37b501e3f3
Lorenzo Stoakes <lstoakes@gmail.com>
2022-03-07 22:37:11 +0000

    build: significantly expand build command

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+set -e; set -o pipefail
 
-pdflatex -shell-escape book.tex
+# Generate PDF of book.
+function generate()
+{
+	pdflatex -shell-escape book.tex
+}
+
+# Run twice to generate dynamic data like ToC.
+generate
+biber book
+makeindex book
+generate
+generate
