e93c31630aa269f960af020fe524b21c425e7676
Lorenzo Stoakes <lstoakes@gmail.com>
2022-03-09 22:48:24 +0000

    script: do initial build on watch, include figures dir

--- watch.sh
+++ watch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e; set -o pipefail
 
-while inotifywait -e close_write -r book.tex chapters/; do
+# Do initial build.
+./build.sh
+# Now watch for file changes.
+while inotifywait -e close_write -r book.tex chapters/ figures/; do
 	./build.sh
 done
